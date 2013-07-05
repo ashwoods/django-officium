9ae3d7b5d15260e154fa1e4dce43f117804d2396
Ashley Camba Garrido <ashwoods@gmail.com>
2013-07-05 17:57:30 +0200

    adding decorator

--- officium/decorators.py
+++ officium/decorators.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 from decorator import decorator
+import importlib
+
 from officium.conf import settings
 
 
-def officium(label):
-    def officum(f, *args, **kwargs):
-        return f(*args, **kwargs)
 
-    return decorator(officium)
+def officium_method(relation, label=None):
+    def call(self, object, officium_relation=relation, officium_label=label, *args, **kwargs):
+        function = self
+        officium = reduce(getattr, officium_relation.split('.'), object)
+
+        try:
+            function = getattr(importlib.import_module('%s.%s' % (settings.OFFICIUM_DECORATOR_MODULE, officium.slug)),
+                               officium_label)
+        except (AttributeError, ImportError):
+            pass
+
+        return function(object, *args, **kwargs)
+    return decorator(call)
 
